15f1e6ed9abd5faa5203c4570bcc65131e1e93e3
eideard-hm <ehhernandez81@misena.edu.co>
2022-01-24 08:29:42 -0500

    Modulos y componentes iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroesModule } from './heroes/heroes.module';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
