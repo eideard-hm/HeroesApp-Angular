a7aa6d742c8911f2805247f4ded869722b8b8e48
eideard-hm <ehhernandez81@misena.edu.co>
2022-01-25 09:18:06 -0500

    Added the service

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Observable } from 'rxjs';
+import { Heroes } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
@@ -8,9 +11,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroes[] = [];
+
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+    this.heroesService.getHeroes()
+    .subscribe(heroe => this.heroes = heroe)
   }
 
 }
